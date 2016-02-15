b03c3fee90e45a78c67eccb6d814ee179f47f84f
eclipsethemoon <ottohoang@gmail.com>
2016-02-15 09:14:26 -0600

    Alot of changes made

--- TCPclient.py
+++ TCPclient.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python
 
 import socket
+import sys
 
-s = socket.socket()	#create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#create a socket object
 
 HOST = socket.gethostname()
 PORT = 8008
+server_address = (HOST,PORT)
+print >>sys.stderr,'connection to %s port %s' % server_address
+s.connect(server_address)
+try:
+	#send data
+	message = ''
+	print >>sys.stderr, 'sending "%s"' % message
+	s.sendall(message)
+	
+	#look for the response
+	amount_received = 0
+	amount_expected = len(message)
+	
+	while amount_received < amount_expected:
+		data = s.recv(16)
+		amount_received +=len(data)
+		print >>sys.stderr, 'received "%s"' % data
+finally:
+	print >>sys.stderr, 'closing socket'
+	s.close()
 
-s.connect((HOST, PORT))
-print s.recv(1024)
-s.close
--- TCPserver.py
+++ TCPserver.py
@@ -8,13 +8,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 HOST = socket.gethostname() #Get local machine name
 PORT = 8008
-
+server_address = (HOST,PORT)
 
 print '\n200 NBserver version Beta ready '
 
 #Bind socket to local host and port
 try:
-	s.bind((HOST,PORT))
+	s.bind(server_address)
 except socket.error as msg:
 	print 'Bind failed. Error code: '+ str(msg[0])+ ' Message '+ msg[1]
 	sys.exit()
@@ -25,10 +25,22 @@ s.listen(1)
 print 'Socket now listening'
 
 #now keep talking with the client
-while 1:
+while True:
 	#wait to accept a connection - bloacking call
-	conn, addr = s.accept()
-	print 'Connected with ' + addr[0] + ':'+str(addr[1])
-	conn.send('Thank you for connecting')
-	conn.close()
+	print >>sys.stderr,'waiting for a connection'
+	connection, client_address = s.accept()
+	try:
+		print >>sys.stderr,'connecting from', client_address
+	#Recieve the data in small chunks and retransmit it
+		while True:
+			data = connection.recv(16)
+			print >>sys.stderr,'"received "%s"' %data
+			if data:
+				print >>sys.stderr,'sending data back to the client'
+				connection.sendall(data)
+			else:
+				print >>sys.stderr,'no more data from',client_address
+				break
+	finally:
+		connection.close()
 
